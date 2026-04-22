e7be0d2c37f9d3d225b2a3d6f76878b44774bead
catthyy <catthyy25@gmail.com>
2026-04-22 14:22:46 +0100

    Some changes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {TareasComponent} from './components/tareas/tareas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: TareasComponent},
+];
